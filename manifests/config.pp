acf00c3e91504561603577ff0fd7492b40b749c1
Alejandro Alonso Mayo <codereaper91@gmail.com>
2020-01-26 13:51:56 +0100

    feat: adds config template

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,2 +1,7 @@
 class samba::config {
+    file { '/etc/samba/smb.conf':
+        ensure => present,
+        require => Class['samba::install'],
+        content => epp('samba/smb.conf.epp')
+    }
 }
