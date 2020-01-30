4bed1c0c5a5fa41defb9a9692d9ec2a2e6e86cba
Alejandro Alonso Mayo <codereaper91@gmail.com>
2020-01-30 21:53:59 +0100

    feat: adds basic public share definitions

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,9 +1,25 @@
 class samba::config {
   if $samba::config_manage {
+    $share_definitions = [
+      {
+        name => 'public',
+        path => '/srv/public',
+      }
+    ]
+
+    $share_definitions.each |$index, $value| {
+      file {$value[path]:
+        ensure => directory,
+        mode => "1777",
+      }
+    }
+
     file { '/etc/samba/smb.conf':
-        require => Class['samba::install'],
-        ensure => present,
-        content => epp('samba/smb.conf.epp')
+      require => Class['samba::install'],
+      ensure => present,
+      content => epp('samba/smb.conf.epp', {
+        share_definitions => $share_definitions
+      })
     }
   }
 }
