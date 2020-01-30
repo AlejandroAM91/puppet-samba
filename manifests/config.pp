c0c0c56dc551ea569ba0d01ef26514ab9c5e280f
Alejandro Alonso Mayo <alejandroalonsomayo@gmail.com>
2020-01-30 22:16:29 +0100

    Version 0.2.0

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,2 +1,20 @@
 class samba::config {
+  if $samba::config_manage {
+    $share_definitions = $samba::share_definitions
+
+    $share_definitions.each |$index, $value| {
+      file {$value[path]:
+        ensure => directory,
+        mode => "1777",
+      }
+    }
+
+    file { '/etc/samba/smb.conf':
+      require => Class['samba::install'],
+      ensure => present,
+      content => epp('samba/smb.conf.epp', {
+        share_definitions => $share_definitions
+      })
+    }
+  }
 }
